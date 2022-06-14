b041e7615702236af5054014b9fee0b9c75289f8
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-06-14 19:36:26 -0400

    commit tree ee7b8057c1895c396e6f7b169bf5686ac1fff61d

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,8 +13,17 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = {firstName: firstName, lastName: lastName}
+    const dataArray = [...submittedData, formData]
+    setSubmittedData(dataArray)
+    setFirstName("")
+    setLastName("")
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
